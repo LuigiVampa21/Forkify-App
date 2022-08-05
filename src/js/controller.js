5ea8699f4319b9bf3930bb475d9c3741a3627e46
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-08-05 16:31:32 +0200

    ntn

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,54 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultView from './views/resultView.js';
+import View from './views/view.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+import 'regenerator-runtime/runtime';
+import 'core-js/stable';
+import recipeView from './views/recipeView.js';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    // 1 Loading Recipe
+    await model.loadRecipe(id);
+
+    // 2 Rendering Recipe
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+    recipeView.renderError();
+  }
 };
 
-// https://forkify-api.herokuapp.com/v2
+const controlSearchResults = async function () {
+  try {
+    resultView.renderSpinner();
+    // 1 Get Search Query
+    const query = searchView.getQuery();
+    if (!query) return;
+    // 2 Load Research
+    await model.loadSearchResults(query);
+    // 3 Render Results
+    // resultView.render(model.state.search.results);
+    resultView.render(model.getSearchResultsPage());
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-///////////////////////////////////////
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
---
+++ src/js/model.js
@@ -0,0 +1,61 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+import { RES_PER_PAGE } from './config.js';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+    resultsPerPage: RES_PER_PAGE,
+    page: 1,
+  },
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(r => {
+      return {
+        id: r.id,
+        title: r.title,
+        publisher: r.publisher,
+        image: r.image_url,
+      };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage; // 0;
+  const end = page * state.search.resultsPerPage; // 9;
+  return state.search.results.slice(start, end);
+};
